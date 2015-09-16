e9dd1b73ac55dfdb27d4d0ece6f05d56ba0d686b
Kristina Hager <kristina.hager@gmail.com>
2015-09-16 17:29:25 -0500

    compiling with hello planet dependency

---
+++ src/main/java/com/solar/PlanetarySystem.java
@@ -0,0 +1,22 @@
+package com.space;
+
+/**
+ *
+ */
+public class PlanetarySystem {
+
+    String name;
+    //List<Planet> planets = new ArrayList<>();
+
+    /**
+     *
+     */
+    public PlanetarySystem(String name) {
+        this.name = name;
+    }
+
+    public String getName() { 
+        return name;
+    }
+
+}
